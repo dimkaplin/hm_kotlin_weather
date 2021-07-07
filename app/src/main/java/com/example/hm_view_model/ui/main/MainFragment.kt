e18687ea39d3892fc910cd854004fb08e002ba13
Kaplin Dima <Korvin777KDS@mail.ru>
2021-07-07 14:50:59 +0300

    try to load weaher

--- app/src/main/java/com/example/hm_view_model/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/hm_view_model/ui/main/MainFragment.kt
@@ -1,23 +1,37 @@
 package com.example.hm_view_model.ui.main
 
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
 import com.example.hm_view_model.R
 import com.example.hm_view_model.databinding.MainFragmentBinding
 import com.example.hm_view_model.model.ApplState
 import com.example.hm_view_model.model.Weather
+import com.example.hm_view_model.model.WeatherDTO
 import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.stream.Collectors
+import javax.net.ssl.HttpsURLConnection
 
+private const val WEATHER_API_KEY = "a57ed224-c226-4ccb-bd2b-500720e134ba"
 class MainFragment : Fragment() {
 
     private lateinit var binding: MainFragmentBinding
-    
+    private lateinit var viewModel: MainViewModel
+    private lateinit var weatherBundle: Weather
 
     companion object {
         const val BUNDLE_EXTRA = "weather"
@@ -29,7 +43,66 @@ class MainFragment : Fragment() {
         }
     }
 
-    private lateinit var viewModel: MainViewModel
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            main.visibility = View.VISIBLE
+            loadLayout.visibility = View.GONE
+            val city = weatherBundle.city
+            cityName.text = city.city
+            cityCoordinates.text = String.format(
+                getString(R.string.city_coordinates),
+                city.lat.toString(),
+                city.lon.toString()
+            )
+            weatherCondition.text = weatherDTO.fact?.condition
+            temperatureValue.text = weatherDTO.fact?.temp.toString()
+            feelsLikeValue.text = weatherDTO.fact?.feels_like.toString()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun loadWeather() {
+        try {
+            val uri =
+                URL("https://api.weather.yandex.ru/v2/informers?lat=${weatherBundle.city.lat}&lon=${weatherBundle.city.lon}")
+            val handler = Handler()
+            Thread(Runnable {
+                lateinit var urlConnection: HttpsURLConnection
+                try {
+                    urlConnection = uri.openConnection() as HttpsURLConnection
+                    urlConnection.requestMethod = "GET"
+                    urlConnection.addRequestProperty(
+                        "X-Yandex-API-Key",
+                        WEATHER_API_KEY
+                    )
+                    urlConnection.readTimeout = 10000
+                    val bufferedReader =
+                        BufferedReader(InputStreamReader(urlConnection.inputStream))
+
+                    // преобразование ответа от сервера (JSON) в модель данных (WeatherDTO)
+                    val weatherDTO: WeatherDTO =
+                        Gson().fromJson(getLines(bufferedReader), WeatherDTO::class.java)
+                    handler.post { displayWeather(weatherDTO) }
+                } catch (e: Exception) {
+                    Log.e("", "Fail connection", e)
+                    e.printStackTrace()
+                    //Обработка ошибки
+                } finally {
+                    urlConnection.disconnect()
+                }
+            }).start()
+        } catch (e: MalformedURLException) {
+            Log.e("", "Fail URI", e)
+            e.printStackTrace()
+            //Обработка ошибки
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getLines(reader: BufferedReader): String {
+        return reader.lines().collect(Collectors.joining("\n"))
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,7 +113,8 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View,  savedInstanceState: Bundle?) {
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //val observe = Observer<Any> {renderData(it)}
         //viewModel.getData().observe(viewLifecycleOwner, observe)
@@ -60,7 +134,13 @@ class MainFragment : Fragment() {
             )
             binding.temperatureValue.text = weather.temperature.toString()
             binding.feelsLikeValue.text = weather.feelsLike.toString()
-        }
+        }//оставил потому что онлайн пока не срабатывает
+
+        weatherBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Weather()
+      //  binding.main.visibility = View.GONE
+      //  binding.loadLayout.visibility = View.VISIBLE
+        loadWeather()
+
 
     }
 
